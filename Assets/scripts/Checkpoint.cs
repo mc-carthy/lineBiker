5cce8843665e929fc343149fd87fe00b29162295
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-15 16:16:33 +0000

    Implement checkpoint flags

--- Assets/scripts/Checkpoint.cs
+++ Assets/scripts/Checkpoint.cs
@@ -2,6 +2,23 @@
 
 public class Checkpoint : MonoBehaviour {
 
-	
+    [SerializeField]
+    private Sprite hitCheckPoint;
+
+    private SpriteRenderer sprRen;
+	private bool isHit;
+
+    private void Awake ()
+    {
+        sprRen = GetComponent<SpriteRenderer> ();
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag ("Player"))
+        {
+            sprRen.sprite = hitCheckPoint;
+        }
+    }
 
 }
