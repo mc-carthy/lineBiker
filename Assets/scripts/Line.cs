a0c42afdf3e42f022f2e78ebf9b51cf676aa55d6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-15 15:25:49 +0000

    Spawn line with mouse input

---
+++ Assets/scripts/Line.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Line : MonoBehaviour {
+
+    private LineRenderer lineRen;
+    private EdgeCollider2D edgeCol;
+
+    private List<Vector2> points;
+    private float pointDistanceThreshold = 0.1f;
+
+    private void Awake ()
+    {
+        lineRen = GetComponent<LineRenderer> ();
+        edgeCol = GetComponent<EdgeCollider2D> ();
+    }
+
+    public void UpdateLine (Vector2 point)
+    {
+        if (points == null)
+        {
+            points = new List<Vector2> ();
+            SetPoint (point);
+            return;
+        }
+
+        if (Vector2.Distance (points.Last (), point) > pointDistanceThreshold)
+        {
+            SetPoint (point);
+        }
+    }
+
+    private void SetPoint (Vector2 point)
+    {
+        points.Add (point);
+
+        lineRen.numPositions = points.Count;
+        lineRen.SetPosition (points.Count - 1, point);
+
+        if (points.Count > 1)
+        {
+            edgeCol.points = points.ToArray ();
+        }
+    }
+
+}
---
+++ Assets/scripts/LineManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LineManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject linePrefab;
+
+    private Line currentLine;
+
+	private void Update ()
+    {
+        if (Input.GetMouseButtonDown (0))
+        {
+            GameObject newLine = Instantiate (linePrefab) as GameObject;
+            currentLine = newLine.GetComponent<Line> ();
+        }
+    }
+
+}
