6064efe547c8e5b2c380e21ab57156abe53229d1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-15 15:30:59 +0000

    Player released on correct input

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.Space))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+}
